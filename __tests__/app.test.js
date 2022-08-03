819d58db1b2db944afe8929bef445a342ddc5a3b
KASHI TAMANG <tamang.kashi@gmail.com>
2022-08-03 09:55:39 -0700

    writing test for GET route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,15 +1,23 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+ 
+  it('GET /books should return a list of books', async () => {
+    const res = await request(app).get('/books');
+    expect(res.body.length).toEqual(10);
+    expect(res.body[0]).toEqual(
+      {
+        id: expect.any(Number),
+        title: expect.any(String)
+      });
   });
+
   afterAll(() => {
     pool.end();
   });
