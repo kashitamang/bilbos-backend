5942d7b350eb841fd3989be8a6a5e23b88a9ce9f
KASHI TAMANG <tamang.kashi@gmail.com>
2022-08-03 10:04:23 -0700

    adding book model, controller, and attaching route to app. passes test and renders to the page

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,14 +7,15 @@ describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
- 
+
   it('GET /books should return a list of books', async () => {
     const res = await request(app).get('/books');
     expect(res.body.length).toEqual(10);
     expect(res.body[0]).toEqual(
       {
-        id: expect.any(Number),
-        title: expect.any(String)
+        id: expect.any(String),
+        title: expect.any(String),
+        released: expect.any(String)
       });
   });
 
